c45b352ffef6c4eb498e4af49ff0e5cc06de2121
rakib hasan <rakibhasanmd457@gmail.com>
2025-07-24 08:59:24 +0600

    make sucssalfully

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 export default function Home() {
   return (
     <div>
-        
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Body from './Component/Body/Body';
+import Home from './Component/Home/Home';
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<div>hello</div>
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        index: true,
+        element: <Body></Body>
+      }
+    ]
   }
 ])
 
