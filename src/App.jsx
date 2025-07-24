e029ff0ab79d7add5d2181699b069e8a5af5d9fd
rakib hasan <rakibhasanmd457@gmail.com>
2025-07-24 08:25:32 +0600

    create the router project and set up tailwind, dasi ui setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <button className="btn btn-primary">Click me</button>
+      <h1 className="text-3xl font-bold text-red-500">rakib</h1>
+    </>
+  );
+}
+export default App;
